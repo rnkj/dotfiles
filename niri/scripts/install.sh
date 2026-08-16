74c030541971cb369657bd44d866df543053246a
Ryoichi Nakajo <ryoichi-nakajo@aist.go.jp>
2026-08-16 17:41:46 +0900

    [Niri] Update dependencies

--- niri/scripts/install.sh
+++ niri/scripts/install.sh
@@ -6,4 +6,10 @@ sudo add-apt-repository ppa:avengemedia/danklinux
 sudo add-apt-repository ppa:avengemedia/dms
 sudo aptitude install -y niri dms
 
+# thunar
+sudo aptitude install -y thunar
 
+# yazi 
+curl -fsSL https://yazi-rs.github.io/builds/yazi-keyring.gpg | sudo tee /usr/share/keyrings/yazi-keyring.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/yazi-keyring.gpg] https://yazi-rs.github.io/builds/ stable main' | sudo tee /etc/apt/sources.list.d/yazi.list >/dev/null
+sudo apt update && sudo aptitude install -y yazi
